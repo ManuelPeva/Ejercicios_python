2760c99a5c6cb477a9bdb1f009c2eb13b13d93b3
manuel_859 <manuel_859@outlook.es>
2023-03-19 21:37:30 -0600

    Contador de elementos de una lista

---
+++ longitud_de_lista.py
@@ -0,0 +1,17 @@
+'''Definir una función que calcule la logitud de una lista o una cadena dada
+escribirla sin usar funciones de python'''
+
+#declaración de la lista
+list = [22,3,1,22,8,'hola',29]
+
+#creación de contador en ceros
+conta = 0
+
+
+#iterador que recorrera la lista
+for i in list:
+    conta += 1
+print(f"La longitud de la lista es: {conta}")
+
+
+
