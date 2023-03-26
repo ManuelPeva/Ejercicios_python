24d18cf7387b39902c70279f9d800fd5b1582288
manuel_859 <manuel_859@outlook.es>
2023-03-26 16:14:57 -0600

    fix: Formas de imprimir en python

---
+++ exit.py
@@ -0,0 +1,11 @@
+#salidas
+
+nombre = 'manuel'
+edad = 22
+
+print(f"Mi nombre es: {nombre} y tengo {edad}")
+
+print("mi nombre es:",nombre,"y tengo",edad,"años")
+
+print("mi nombre es: {} y tengo {} años".format(nombre,edad))
+
